1c4a8b2d50b3518f378621ea50d3c8eacd499e84
Bertrand Guay-Paquet <berniegp@gmail.com>
2022-06-22 10:07:19 +0200

    Migrate to TypeScript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,31 @@
+const { rules: tsBaseRules } = require('eslint-config-airbnb-typescript/lib/shared');
+
 module.exports = {
   root: true,
-  extends: 'airbnb-base',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+  ],
   rules: {
     // Allow to use braces if desired
     'arrow-body-style': 0,
 
-    // Generally makes sense, but too strict to enforce
-    'class-methods-use-this': 0,
-
     // Trailing commas on function arguments is just silly
-    'comma-dangle': ['error', {
-      arrays: 'always-multiline',
-      objects: 'always-multiline',
-      imports: 'always-multiline',
-      exports: 'always-multiline',
+    '@typescript-eslint/comma-dangle': ['error', {
+      ...tsBaseRules['@typescript-eslint/comma-dangle'][1],
       functions: 'never',
     }],
 
     // Saving 2 characters is not worth the potential errors
-    'curly': 'error',
+    'curly': ['error', 'all'],
 
     // A chain of 'if' and 'else if' statements is clearer than multiple individual 'if' blocks
     'no-else-return': ['error', { allowElseIf: true }],
@@ -30,7 +37,7 @@ module.exports = {
     'no-plusplus': 0,
 
     // Finding good names is hard so allow reuse
-    'no-shadow': 0,
+    '@typescript-eslint/no-shadow': 0,
 
     // This is still the best way to express the private api intent
     'no-underscore-dangle': ['error', { 'allowAfterThis': true }],
@@ -38,7 +45,10 @@ module.exports = {
     // Allow functions to be used before defined because:
     // 1) they are hoisted;
     // 2) it allows code ordering that moves helper functions to the bottom.
-    'no-use-before-define': ['error', { functions: false, classes: true, variables: true }],
+    '@typescript-eslint/no-use-before-define': ['error', {
+      ...tsBaseRules['@typescript-eslint/no-use-before-define'][1],
+      functions: false,
+    }],
 
     // To allow comment blocks that are actually section headers
     'spaced-comment': ['error', 'always', { 'exceptions': ['/'] }],
